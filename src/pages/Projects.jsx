d0e39043afa041aa99d04def0beb661217f1a214
Harendra Godara <harendragodara880@gmail.com>
2026-05-25 10:23:55 +0530

    update project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,29 +1,20 @@
 import ProjectCard from "../components/ProjectCard";
 
 export default function Projects({ projects }) {
+  return (
+    <div className="projects-page">
+      <h1>My Projects</h1>
 
-    return (
-
-        <div className="projects-page">
-
-            <h1>My Projects</h1>
-
-            <div className="projects-container">
-
-                {
-                    projects.map((item, index) => (
-
-                        <ProjectCard
-                            key={index}
-                            title={item.title}
-                            desc={item.desc}
-                        />
-
-                    ))
-                }
-
-            </div>
-
-        </div>
-    )
+      <div className="projects-container">
+        {projects.map((item, index) => (
+          <ProjectCard
+            key={index}
+            title={item.title}
+            desc={item.desc}
+            link={item.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
